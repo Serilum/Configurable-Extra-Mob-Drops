ad2e1d089ca1b4315284b91c0000142df9fb04cd
Rick South <rick@natamus.com>
2023-12-12 10:41:12 +0100

    Configurable Extra Mob Drops source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/configurableextramobdrops/events/MobDropEvent.java
@@ -0,0 +1,62 @@
+package com.natamus.configurableextramobdrops.events;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.natamus.collective.data.GlobalVariables;
+import com.natamus.configurableextramobdrops.util.Util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class MobDropEvent {
+	public static void mobItemDrop(Level world, Entity entity, DamageSource damageSource) {
+		if (world.isClientSide) {
+			return;
+		}
+		
+		EntityType<?> entitytype = entity.getType();
+		if (!Util.mobdrops.containsKey(entitytype)) {
+			return;
+		}
+		
+		CopyOnWriteArrayList<ItemStack> extradrops = Util.mobdrops.get(entitytype);
+		if (extradrops.size() > 0) {
+			BlockPos epos = entity.blockPosition();
+			
+			for (ItemStack itemstack : extradrops) {
+				ItemStack newstack = itemstack.copy();
+				CompoundTag tag = newstack.getOrCreateTag();
+				
+				CompoundTag nbt = new CompoundTag();
+				newstack.save(nbt);
+				
+				
+				if (tag.contains("dropchance")) {
+					double dropchance = tag.getDouble("dropchance");
+					if (dropchance != 1.0) {
+						double chanceroll = GlobalVariables.random.nextDouble();
+						if (chanceroll > dropchance) {
+							continue;
+						}
+					}
+					tag.remove("dropchance");
+					if (tag.size() == 0) {
+						nbt.remove("tag");
+						newstack = ItemStack.of(nbt);
+					}
+					else {
+						newstack.setTag(tag);
+					}
+				}
+				
+				world.addFreshEntity(new ItemEntity(world, epos.getX(), epos.getY()+1, epos.getZ(), newstack.copy()));
+			}
+		}
+	}
+}
---
+++ Fabric/src/main/java/com/natamus/configurableextramobdrops/ModFabric.java
@@ -0,0 +1,47 @@
+package com.natamus.configurableextramobdrops;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.configurableextramobdrops.cmd.CommandCemd;
+import com.natamus.configurableextramobdrops.events.MobDropEvent;
+import com.natamus.configurableextramobdrops.util.Reference;
+import com.natamus.configurableextramobdrops.util.Util;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		try {
+			Util.loadMobConfigFile();
+		} catch (Exception ex) {
+			System.out.println("[" + Reference.NAME + "] Error on loading the entity config file. The mod has been disabled.");
+			return;
+		}
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			CommandCemd.register(dispatcher);
+		});
+
+		CollectiveEntityEvents.ON_ENTITY_IS_DROPPING_LOOT.register((Level world, Entity entity, DamageSource damageSource) -> {
+			MobDropEvent.mobItemDrop(world, entity, damageSource);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
