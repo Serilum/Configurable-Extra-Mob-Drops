5bfb1eb5a841b554f403f12f7aa74298a3139954
Rick South <rick@natamus.com>
2023-12-12 10:41:17 +0100

    Configurable Extra Mob Drops source code for 1.19.2.

---
+++ Forge/src/main/java/com/natamus/configurableextramobdrops/forge/events/ForgeMobDropEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.configurableextramobdrops.forge.events;
+
+import com.natamus.configurableextramobdrops.cmd.CommandCemd;
+import com.natamus.configurableextramobdrops.events.MobDropEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeMobDropEvent {
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent e) {
+    	CommandCemd.register(e.getDispatcher());
+    }
+
+	@SubscribeEvent
+	public void mobItemDrop(LivingDropsEvent e) {
+		Entity entity = e.getEntity();
+		MobDropEvent.mobItemDrop(entity.level, entity, e.getSource());
+	}
+}
